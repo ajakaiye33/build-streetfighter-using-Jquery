bbfdd7627bfc5c14227c1b412c93186c20dba8e0
ajakaiye33 <ajakzhedgar@gmail.com>
2016-01-10 22:13:37 +0100

    animation effects applied

--- js/app.js
+++ js/app.js
@@ -8,3 +8,42 @@ $(document).ready(function () {
 		$(".ryu-still").show();
 	});
 });
+
+$(document).ready(function(){
+
+	$(".ryu").mouseenter(function(){
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+	.mouseleave(function(){
+		$(".ryu-ready").hide();
+		$(".ryu-still").show();
+
+	})
+	.mousedown(function(){
+		//play hadouken sound
+		playHadouken();
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		//introduced an object
+		$(".hadouken").finish().show().animate(
+			{"left": "1020px"},500,
+			function(){
+				$(this).hide();
+				$(this).css("left", "520px");
+			}
+			);
+	// animate it to the right of the screen
+})
+	.mouseup(function(){
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
+
+	});
+});
+
+function playHadouken() {
+	$("#hadouken-sound")[0].volume= 1;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
+}
